2ed004216039ef8e008b35cd98a2e7c1a8030c1f
Daniela4578 <d.kremendka@gmail.com>
2026-05-08 01:46:55 +0300

    db and db connection created

---
+++ src/db/DatabaseConnection.java
@@ -0,0 +1,36 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static final String url = "jdbc:mysql://localhost:3306/pizza";
+    private static final String user = "root";
+    private static final String password = "SanDwitch8";
+
+    private static DatabaseConnection instance;
+    private final Connection connection;
+
+    public DatabaseConnection() throws SQLException {
+        this.connection = DriverManager.getConnection(url, user, password);
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null || !instance.connection.isClosed()) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+}
