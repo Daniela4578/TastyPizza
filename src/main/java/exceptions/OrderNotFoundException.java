08acc1ae8b0c8673de83004cf99f175ecca17c8d
Daniela4578 <d.kremendka@gmail.com>
2026-08-25 23:49:22 +0300

    new custom exception added

---
+++ src/main/java/exceptions/OrderNotFoundException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) { super(message); }
+}
---
+++ src/main/java/exceptions/PaymentNotFoundException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class PaymentNotFoundException extends RuntimeException {
+    public PaymentNotFoundException(String message) { super(message); }
+}
